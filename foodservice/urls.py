6704ef1b31ad6635d7c601756f53f727cceefc9e
Meir S. List <simlist@gmail.com>
2017-11-07 22:39:09 -0500

    Added urls file to shop and added to app.

--- foodservice/urls.py
+++ foodservice/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^shop/', include('shop.urls')),
     url(r'accounts/', include('authtools.urls')),
 ]
