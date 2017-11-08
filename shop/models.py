ad7efc6ab354e0cd709754fc28a82f5828e7bfa1
Meir S. List <simlist@gmail.com>
2017-11-07 23:11:07 -0500

    Fixed some typos in shop models.

--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Category(models):
-    name = models.Charfield(maxlength=20)
+    name = models.CharField(max_length=20)
     slug = models.SlugField(max_length=30)
 
