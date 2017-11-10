e9c556631fcb9cd5b13c9c6e95a4fc28aa6ad77d
simlist <simlist@gmail.com>
2017-11-10 09:54:11 -0500

    Fixed subclassed module instead of class.

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Category(models):
+class Category(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField(max_length=30)
 
