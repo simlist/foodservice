2555955e6c60ce2634b7f6b96ca1e019ee477e0f
Meir S. List <simlist@gmail.com>
2017-11-12 19:59:25 -0500

    Created Product and related models.

--- shop/models.py
+++ shop/models.py
@@ -7,3 +7,21 @@ class Category(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField(max_length=30)
 
+
+class ProductVariant(models.Model):
+    name = models.CharField(max_length=20)
+    group_name = models.CharField(max_length=20)
+    price_added = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=40)
+    desription = models.TextField()
+    baseprice = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField()
+    category = models.ForeignKey(Category)
+    variant = models.ManyToManyField(ProductVariant)
+
+
+
