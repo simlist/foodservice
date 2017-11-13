511624bb000730eca5ba11c14eb74289ddfc2df4
Meir S. List <simlist@gmail.com>
2017-11-12 23:09:46 -0500

    Created json index for demo.

--- shop/views.py
+++ shop/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Product
+
+def index(request):
+    data = serializers.serialize('json', Product.objects.all(),
+                                 use_natural_foreign_keys=True)
+    return JsonResponse(data)
