d0acb7ad3ea209a83a08b94afdee7714fe98ddcc
Meir S. List <simlist@gmail.com>
2017-11-14 08:28:45 -0500

    Added Metas to models.

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,12 @@ class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
     slug = models.SlugField(max_length=30, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
     def natural_key(self):
         return (self.name,)
 
@@ -14,7 +20,11 @@ class Category(models.Model):
 class ProductVariant(models.Model):
     name = models.CharField(max_length=20)
     group_name = models.CharField(max_length=20)
-    price_added = models.DecimalField(max_digits=4, decimal_places=2)
+    price_added = models.DecimalField(max_digits=4, decimal_places=2,
+                                      default=0.00)
+
+    def __str__(self):
+        return self.name
 
     def natural_key(self):
         return (self.group_name, self.name, self.price_added)
@@ -25,9 +35,10 @@ class Product(models.Model):
     slug = models.SlugField(max_length=40, unique=True)
     description = models.TextField()
     base_price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField()
+    image = models.ImageField(blank=True)
     category = models.ForeignKey(Category)
-    variant = models.ManyToManyField(ProductVariant)
+    variant = models.ManyToManyField(ProductVariant, blank=True)
 
-
+    def __str__(self):
+        return self.name
 
