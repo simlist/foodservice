c9eafe7f8fdd178befee843c7e07ee6ec7459589
Meir S. List <simlist@gmail.com>
2017-11-14 08:30:57 -0500

    Fixed jsonifying json.

--- shop/views.py
+++ shop/views.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.core import serializers
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 from .models import Product
 
 def index(request):
     data = serializers.serialize('json', Product.objects.all(),
                                  use_natural_foreign_keys=True)
-    return JsonResponse(data, safe=False)
+    return HttpResponse(data, content_type='application/json')
