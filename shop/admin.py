f5159e26449d410451cc733a5a6e63b3a1568df8
Meir S. List <simlist@gmail.com>
2017-11-15 08:32:36 -0500

    Changed models to use custom through model.

--- shop/admin.py
+++ shop/admin.py
@@ -3,8 +3,23 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Category, Product
+from .models import Category, Product, Variant
+
+class VariantInline(admin.TabularInline):
+    model = Product.variants.through
+    extra = 2
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name',]}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name',]}
+    inlines = [VariantInline,]
+    exclude = ('variants',)
+
 
 # Register your models here.
-admin.site.register([Category, Product])
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 
--- shop/models.py
+++ shop/models.py
@@ -17,28 +17,40 @@ class Category(models.Model):
         return (self.name,)
 
 
-class ProductVariant(models.Model):
+class Variant(models.Model):
     name = models.CharField(max_length=20)
     group_name = models.CharField(max_length=20)
-    price_added = models.DecimalField(max_digits=4, decimal_places=2,
-                                      default=0.00)
 
     def __str__(self):
         return self.name
 
     def natural_key(self):
-        return (self.group_name, self.name, self.price_added)
+        return (self.group_name, self.name)
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-    slug = models.SlugField(max_length=40, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=70, unique=True)
     description = models.TextField()
     base_price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(blank=True)
     category = models.ForeignKey(Category)
-    variant = models.ManyToManyField(ProductVariant, blank=True)
+    variants = models.ManyToManyField(Variant, through='Item', blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variant = models.ForeignKey(Variant, blank=True, on_delete=models.CASCADE)
+    price_added = models.DecimalField(max_digits=5, decimal_places=2,
+                                      default=0.00)
+
+    def __str__(self):
+        return ' - '.join((self.product.name, self.variant.name))
+
+    @property
+    def price(self):
+        return self.product.base_price + self.price_added
 
