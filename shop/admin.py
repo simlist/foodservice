d9dacfbd331bd366db5984bbe69804c3fc55bf58
Meir S. List <simlist@gmail.com>
2017-11-15 19:37:05 -0500

    Added more models to admin.

--- shop/admin.py
+++ shop/admin.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Category, Product, Variant
+from .models import Category, Product, Variant, Item
 
 class VariantInline(admin.TabularInline):
     model = Product.variants.through
     extra = 2
+
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name',]}
@@ -15,11 +16,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name',]}
-    inlines = [VariantInline,]
+    inlines = [VariantInline, ]
     exclude = ('variants',)
 
 
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
-
+admin.site.register(Variant)
+admin.site.register(Item)
