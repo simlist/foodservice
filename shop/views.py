e7da64fe76bfb4964bdb53c7307d4e1b6fce963d
Meir S. List <simlist@gmail.com>
2017-11-15 22:29:13 -0500

    Created product view.

--- shop/views.py
+++ shop/views.py
@@ -11,3 +11,9 @@ def index(request):
     data = serializers.serialize('json', Product.objects.all(),
                                  use_natural_foreign_keys=True)
     return HttpResponse(data, content_type='application/json')
+
+def product(request):
+    if request.method == 'GET':
+        product = Product.objects.filter(slug=request.GET.get('slug'))
+        data = serializers.serialize('json', product)
+        return HttpResponse(data, content_type='application/json')
