70727037fe5c7db31d833bc13d0448808d6fa18d
Meir S. List <simlist@gmail.com>
2017-11-19 23:10:28 -0500

    Created foodservice url for root to shop.index.

--- foodservice/urls.py
+++ foodservice/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from shop.views import index
+
+app_name = 'foodservice'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^shop/', include('shop.urls')),
     url(r'accounts/', include('authtools.urls')),
+    url(r'^$', index),
 ]
