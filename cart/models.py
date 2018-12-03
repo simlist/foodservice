b28bc7d296ed928bc48e61efb6408dd34278d33c
simlist <simlist@gmail.com>
2018-12-02 22:23:37 -0500

    Rearanged models order.

--- cart/models.py
+++ cart/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Line(models.Model):
-    cart = models.ForeignKey('cart.Cart')
-    item = models.ForeignKey('shop.Item', on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+class Cart(models.Model):
+    time = models.DateTimeField(auto_now=True)
 
+    def __iter__(self):
+        for line in Cart.lines.all():
+            yield line
+
+    def __str__(self):
+        print('cart', self.time)
+
+
+class Line(models.Model):
+    cart = models.ForeignKey(Cart, related_name='lines', on_delete=models.CASCADE)
+    item = models.ForeignKey('shop.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
     def __string__(self):
         return str(self.item)
 
     def price(self):
         return self.item.price() * self.quantity
-
-
-class Cart(models.Model):
-    time = models.DateTimeField(auto_now=True)
